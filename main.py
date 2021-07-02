166a6c65e1d3c061d12c5954c6ea17ead6e5cfe2
Lemuriets <madyarovegor@gmail.com>
2021-07-02 21:05:55 +0500

    был удален html файл из html_files

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from songs_parser import Parser
 
 
 def main() -> None:
-    try:
-        Parser('https://www.amalgama-lab.com/songs/t/twenty_one_pilots/nico_and_the_niners.html', True)
-    except KeyboardInterrupt:
-        print('вы успешно вышли из программы')
+    Parser('https://www.amalgama-lab.com/songs/t/twenty_one_pilots/nico_and_the_niners.html', True)
 
 
 if __name__  == '__main__':
